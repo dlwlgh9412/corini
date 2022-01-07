06e8ede201a603a6665f0cbf03e1227be1b682d7
Jiho <dlwlgh9412@gmail.com>
2022-01-07 13:47:46 +0900

    업데이트 메서드 삭제 및 mapAndSave메서드 삭제
    업데이트 메서드 삭제로 인해 mapAndSave메서드는 더이상 필요없어짐으로 인해 insertNotices 메서드로 통합

--- module-crawler/src/main/java/com/crawler/service/UpbitNoticeService.java
+++ module-crawler/src/main/java/com/crawler/service/UpbitNoticeService.java
@@ -2,11 +2,13 @@ package com.crawler.service;
 
 import com.common.entity.Exchange;
 import com.common.entity.Notice;
+import com.common.repository.ExchangeRepository;
 import com.common.repository.NoticeRepository;
 import com.crawler.client.upbit.UpbitNoticeRestClient;
 import com.crawler.client.upbit.dto.UpbitNoticeInfo;
 import com.crawler.client.upbit.dto.UpbitNoticeResponse;
 import com.crawler.config.UrlProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,56 +17,42 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class UpbitNoticeService {
     private final NoticeRepository noticeRepository;
     private final UpbitNoticeRestClient upbitNoticeRestClient;
+    private final ExchangeRepository exchangeRepository;
     private final UrlProperties urlProperties;
-    private final Exchange exchange;
 
-    public UpbitNoticeService(NoticeRepository noticeRepository, UpbitNoticeRestClient upbitNoticeRestClient, UrlProperties urlProperties) {
+    public UpbitNoticeService(NoticeRepository noticeRepository, ExchangeRepository exchangeRepository, UpbitNoticeRestClient upbitNoticeRestClient, UrlProperties urlProperties) {
         this.noticeRepository = noticeRepository;
+        this.exchangeRepository = exchangeRepository;
         this.upbitNoticeRestClient = upbitNoticeRestClient;
         this.urlProperties = urlProperties;
-        this.exchange = Exchange.builder()
-                .exchangeType(Exchange.ExchangeType.UPBIT)
-                .overSea(false)
-                .url(urlProperties.getUpbitUrl())
-                .build();
-    }
 
-    public void checkLastNoticeId() {
-        BigDecimal lastNoticeId = noticeRepository.getLastNoticeIdByExchange(Exchange.ExchangeType.UPBIT.getKey()).orElse(BigDecimal.ZERO);
     }
 
     @Transactional
-    public void searchNotice(BigDecimal lastNoticeId, int page) {
-        UpbitNoticeResponse response = upbitNoticeRestClient.getNotices(page);
+    public void searchNotice(int per_page) {
+        UpbitNoticeResponse response = upbitNoticeRestClient.getNotices(per_page);
+        BigDecimal lastNoticeId = noticeRepository.getLastNoticeIdByExchange(Exchange.ExchangeType.UPBIT.getKey()).orElse(BigDecimal.ZERO);
 
         if (response.getSuccess()) {
-            insertNotices(response.getData().getList(), lastNoticeId);
-            updateFixedNotices(response.getData().getFixedNotices());
+            for (int page = 1; page <= response.getData().getTotalPages(); page++) {
+                response = upbitNoticeRestClient.getNotices(page, per_page);
+                if (insertNotices(response.getData().getList(), lastNoticeId) < per_page)
+                    break;
+            }
         } else {
-            // 커스텀 예외로 수정
+            // 사용자정의 예외로 수정
             throw new RuntimeException();
         }
     }
 
-    public List<Integer> findOutPageList() {
-
-    }
-
-    private void insertNotices(List<UpbitNoticeInfo> noticeInfoList, BigDecimal lastNoticeId) {
+    private Integer insertNotices(List<UpbitNoticeInfo> noticeInfoList, BigDecimal lastNoticeId) {
         noticeInfoList = noticeInfoList.stream().filter(noticeInfo -> BigDecimal.valueOf(noticeInfo.getId()).compareTo(lastNoticeId) > 0).collect(Collectors.toList());
-        mapAndSave(noticeInfoList);
-    }
-
-    private void updateFixedNotices(List<UpbitNoticeInfo> noticeInfoList) {
-        noticeInfoList = noticeInfoList.stream().filter(noticeInfo -> BigDecimal.valueOf(noticeInfo.getId()).compareTo(BigDecimal.valueOf(noticeInfo.getId())) <= 0).collect(Collectors.toList());
-        mapAndSave(noticeInfoList);
-    }
-
-    private void mapAndSave(List<UpbitNoticeInfo> noticeInfoList) {
+        Exchange exchange = selectExchange();
         List<Notice> noticeList = noticeInfoList.stream().map(noticeInfo -> Notice.builder()
                 .noticeId(BigDecimal.valueOf(noticeInfo.getId()))
                 .exchange(exchange)
@@ -74,7 +62,17 @@ public class UpbitNoticeService {
                 .createdDate(ZonedDateTime.parse(noticeInfo.getCreatedAt()))
                 .updatedDate(ZonedDateTime.parse(noticeInfo.getUpdatedAt()))
                 .build()).collect(Collectors.toList());
-        noticeRepository.saveAll(noticeList);
+
+        return noticeRepository.saveAll(noticeList).size();
+    }
+
+    private Exchange selectExchange() {
+        Exchange exchange = exchangeRepository.findById(Exchange.ExchangeType.UPBIT).orElse(Exchange.builder()
+                .name(Exchange.ExchangeType.UPBIT)
+                .overSea(false)
+                .url(urlProperties.getUpbitUrl())
+                .build());
+        return exchange;
     }
 
     private Notice.NoticeKind discriminateNoticeKind(String title) {
