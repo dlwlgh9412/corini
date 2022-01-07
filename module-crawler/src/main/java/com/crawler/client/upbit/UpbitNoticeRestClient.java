3a6eecf5b864755ad6e520f6edfa8bc7e3e1c69a
Jiho <dlwlgh9412@gmail.com>
2022-01-07 13:50:23 +0900

    client - upbit - getNotices메서드 오버로딩 및 per_page 파라미터 변수화

--- module-crawler/src/main/java/com/crawler/client/upbit/UpbitNoticeRestClient.java
+++ module-crawler/src/main/java/com/crawler/client/upbit/UpbitNoticeRestClient.java
@@ -17,11 +17,19 @@ public class UpbitNoticeRestClient {
                 .build();
     }
 
-    public UpbitNoticeResponse getNotices(int page) {
+    public UpbitNoticeResponse getNotices(int perPage) {
         return webClient.get().uri(uriBuilder -> uriBuilder
-                        .path("/notices")
+                        .queryParam("per_page", perPage)
+                        .queryParam("thread_name", "general")
+                        .build())
+                .retrieve()
+                .bodyToMono(UpbitNoticeResponse.class).block();
+    }
+
+    public UpbitNoticeResponse getNotices(int page, int perPage) {
+        return webClient.get().uri(uriBuilder -> uriBuilder
                         .queryParam("page", page)
-                        .queryParam("per_page", 20)
+                        .queryParam("per_page", perPage)
                         .queryParam("thread_name", "general")
                         .build())
                 .retrieve()
