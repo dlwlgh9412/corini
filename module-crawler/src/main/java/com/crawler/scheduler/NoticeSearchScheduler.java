19cc6d4b304df1e9408ba5f3f678d38959905649
Jiho <dlwlgh9412@gmail.com>
2022-01-07 13:49:05 +0900

    스케쥴러 추가

--- module-crawler/src/main/java/com/crawler/scheduler/NoticeSearchScheduler.java
+++ module-crawler/src/main/java/com/crawler/scheduler/NoticeSearchScheduler.java
@@ -1,10 +1,26 @@
 package com.crawler.scheduler;
 
+import com.crawler.service.UpbitNoticeService;
+import com.crawler.service.enums.PerPage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Slf4j
 @Component
 public class NoticeSearchScheduler {
+    private final UpbitNoticeService upbitNoticeService;
 
+    public NoticeSearchScheduler(UpbitNoticeService upbitNoticeService) {
+        this.upbitNoticeService = upbitNoticeService;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?", zone = "Asia/Seoul")
+    public void searchNotice() {
+        log.info("{} 업비트 공지사항 Searching", ZonedDateTime.now(ZoneId.of("Asia/Seoul")));
+        upbitNoticeService.searchNotice(PerPage.UPBIT_DEFAULT.getValue());
+    }
 }
